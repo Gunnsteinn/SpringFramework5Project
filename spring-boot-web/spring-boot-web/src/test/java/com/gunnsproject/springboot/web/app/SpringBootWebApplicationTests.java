a7b4f6aa99a1548c3ff84909e0515329b25a0bf1
Xprox <guillecolo@gmail.com>
2019-12-10 00:05:14 -0300

    Added Project.

---
+++ spring-boot-web/spring-boot-web/src/test/java/com/gunnsproject/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunnsproject.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
