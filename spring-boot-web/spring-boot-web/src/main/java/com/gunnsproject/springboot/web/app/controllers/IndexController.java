4b145f7f113eba54bb8d9445f79973d5294b402d
Xprox <guillecolo@gmail.com>
2019-12-10 00:32:57 -0300

    Added Request map from controller.

--- spring-boot-web/spring-boot-web/src/main/java/com/gunnsproject/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/spring-boot-web/src/main/java/com/gunnsproject/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,18 @@
 package com.gunnsproject.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/index", "/" , "/home"})
-    public String index(){
+    @GetMapping({"/index", "/", "" , "/home"})
+    public String index(Model model){
+        model.addAttribute("title", "Hi Spring Framework!");
         return "index";
     }
 }
